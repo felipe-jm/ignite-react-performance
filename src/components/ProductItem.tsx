098b8f368160a9d095d64bff680ee989d1ca93c4
Felipe Jung <felipemattoseu@gmail.com>
2021-05-11 22:27:41 -0400

    Use lazy loading to load add to wishlist component

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,4 +1,15 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import dynamic from "next/dynamic";
+
+import { AddProductToWishlistProps } from "components/AddProductToWishlist";
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  () =>
+    import("./AddProductToWishlist").then((mod) => mod.AddProductToWishlist),
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
 
 type ProductItemProps = {
   product: {
@@ -17,12 +28,24 @@ type ProductItemProps = {
 const ProductItemComponent = ({
   product,
   onAddToWishlist,
-}: ProductItemProps) => (
-  <div>
-    {product.title} - <strong>{product.priceFormatted}</strong>
-    <button onClick={() => onAddToWishlist(product.id)}>Add to wishlist</button>
-  </div>
-);
+}: ProductItemProps) => {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
+  return (
+    <div>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
+    </div>
+  );
+};
 
 export const ProductItem = memo(
   ProductItemComponent,
